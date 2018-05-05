6426283f15361973c72020c69479babfb8b9d782
Teo <Teo>
2018-05-05 18:01:57 +0300

    Added Comparisons and Groups tables.

---
+++ app/Group.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Teo
+ * Date: 5/5/2018
+ * Time: 4:26 PM
+ */
---
+++ database/migrations/2018_05_05_125306_create_comparisons_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateComparisonsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comparisons', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('homework_id_1');
+            $table->integer('homework_id_2');
+            $table->integer('plagiarism_degree');
+            $table->integer('user_id');
+            $table->enum('type');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comparisons');
+    }
+}
