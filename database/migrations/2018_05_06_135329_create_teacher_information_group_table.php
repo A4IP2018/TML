ae74a9692449d0318698de2e2d39ced8d45d2696
boandrei <bocsan.andrei@gmail.com>
2018-05-06 17:00:20 +0300

    Migrations refactory

--- database/migrations/2018_05_06_135329_create_teacher_information_group_table.php
+++ database/migrations/2018_05_06_135329_create_teacher_information_group_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTeacherInformationGroupsTable extends Migration
+class CreateTeacherInformationGroupTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateTeacherInformationGroupsTable extends Migration
      */
     public function up()
     {
-        Schema::create('teacher_information_groups', function (Blueprint $table) {
+        Schema::create('teacher_information_group', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('group_id');
             $table->integer('teacher_information_id');
@@ -29,6 +29,6 @@ class CreateTeacherInformationGroupsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('teacher_information_groups');
+        Schema::dropIfExists('teacher_information_group');
     }
 }
