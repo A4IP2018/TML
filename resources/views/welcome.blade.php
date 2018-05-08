d5842d88d5fb1ae45b920ab5347fa28647886574
boandrei <bocsan.andrei@gmail.com>
2018-05-09 00:59:12 +0300

    Removed blade text

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -77,20 +77,6 @@
                     @endif
                 </div>
             @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Vlad
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
         </div>
     </body>
 </html>
