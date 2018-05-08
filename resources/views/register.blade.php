0a74ea795a0948e1ea87a4b451d22b3797241546
Denis ROZIMOVSCHII <rozimovschii.denis@gmail.com>
2018-05-09 01:09:42 +0300

    Fixed login and register pages

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,75 +1,53 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.master')
 
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
-  <title>SB Admin - Start Bootstrap Template</title>
-  <!-- Bootstrap core CSS-->
-  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <!-- Custom fonts for this template-->
-  <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-  <!-- Custom styles for this template-->
-  <link href="css/sb-admin.css" rel="stylesheet">
-</head>
-
-<body class="bg-dark">
-  <div class="container">
-    <div class="card card-register mx-auto mt-5">
-      <div class="card-header">Register an Account</div>
-      <div class="card-body">
-        <form method="POST" action="{{ route('register-action') }}">
-          <input type="hidden" name="_token" value="{{ csrf_token() }}">
-          <div class="form-group">
-            <div class="form-row">
-              <div class="col-md-6">
-                <label for="exampleInputName">First name</label>
-                <input name="first-name" class="form-control" id="exampleInputName" type="text" aria-describedby="nameHelp" placeholder="Enter first name">
-              </div>
-              <div class="col-md-6">
-                <label for="exampleInputLastName">Last name</label>
-                <input name="last-name" class="form-control" id="exampleInputLastName" type="text" aria-describedby="nameHelp" placeholder="Enter last name">
-              </div>
+@section('content')
+<div class="container">
+  <div class="card card-register mx-auto mt-5">
+    <div class="card-header">Register an Account</div>
+    <div class="card-body">
+      <form method="POST" action="{{ route('register-action') }}">
+        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+        <div class="form-group">
+          <div class="form-row">
+            <div class="col-md-6">
+              <label for="exampleInputName">First name</label>
+              <input name="first-name" class="form-control" id="exampleInputName" type="text" aria-describedby="nameHelp" placeholder="Enter first name">
+            </div>
+            <div class="col-md-6">
+              <label for="exampleInputLastName">Last name</label>
+              <input name="last-name" class="form-control" id="exampleInputLastName" type="text" aria-describedby="nameHelp" placeholder="Enter last name">
             </div>
           </div>
-          <div class="form-group">
-            <label for="exampleInputEmail1">Email address</label>
-            <input name="email" class="form-control" id="exampleInputEmail1" type="email" aria-describedby="emailHelp" placeholder="Enter email">
-          </div>
-
-          <div class="form-group">
-            <label for="exampleInputEmail1">Username</label>
-            <input name="username" class="form-control" id="register-username" type="text" placeholder="Enter username">
-          </div>
-          <div class="form-group">
-            <div class="form-row">
-              <div class="col-md-6">
-                <label for="exampleInputPassword1">Password</label>
-                <input name="password" class="form-control" id="exampleInputPassword1" type="password" placeholder="Password">
-              </div>
-              <div class="col-md-6">
-                <label for="exampleConfirmPassword">Confirm password</label>
-                <input name="confirm-password" class="form-control" id="exampleConfirmPassword" type="password" placeholder="Confirm password">
-              </div>
-            </div>
-          </div>
-          <button type="submit" class="btn btn-primary btn-block">Register</button>
-        </form>
-        <div class="text-center">
-          <a class="d-block small mt-3" href="login.html">Login Page</a>
-          <a class="d-block small" href="forgot-password.html">Forgot Password?</a>
         </div>
+        <div class="form-group">
+          <label for="exampleInputEmail1">Email address</label>
+          <input name="email" class="form-control" id="exampleInputEmail1" type="email" aria-describedby="emailHelp" placeholder="Enter email">
+        </div>
+
+        <div class="form-group">
+          <label for="exampleInputEmail1">Username</label>
+          <input name="username" class="form-control" id="register-username" type="text" placeholder="Enter username">
+        </div>
+        <div class="form-group">
+          <div class="form-row">
+            <div class="col-md-6">
+              <label for="exampleInputPassword1">Password</label>
+              <input name="password" class="form-control" id="exampleInputPassword1" type="password" placeholder="Password">
+            </div>
+            <div class="col-md-6">
+              <label for="exampleConfirmPassword">Confirm password</label>
+              <input name="confirm-password" class="form-control" id="exampleConfirmPassword" type="password" placeholder="Confirm password">
+            </div>
+          </div>
+        </div>
+        <button type="submit" class="btn btn-primary btn-block">Register</button>
+      </form>
+      <div class="text-center">
+        <a class="d-block small mt-3" href="login.html">Login Page</a>
+        <a class="d-block small" href="forgot-password.html">Forgot Password?</a>
       </div>
     </div>
   </div>
-  <!-- Bootstrap core JavaScript-->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <!-- Core plugin JavaScript-->
-  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-</body>
+</div>
 
-</html>
+@endsection
