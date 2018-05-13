1659a80238bbdcecda167c86d8fb8f032142546a
boandrei <bocsan.andrei@gmail.com>
2018-05-13 12:45:33 +0300

    Added middleware

--- app/Http/Controllers/HomeworkController.php
+++ app/Http/Controllers/HomeworkController.php
@@ -18,6 +18,11 @@ use Symfony\Component\HttpFoundation\Response;
 
 class HomeworkController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['index','show']]);
+    }
+
     /**
      * Display a listing of the resource.
      *
