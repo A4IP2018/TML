e74dafc976c9d71e4e7a46c373e7f0bbbdcca6da
teodoratimcu <teodoratimcu@gmail.com>
2018-05-13 19:53:46 +0300

    Profile page changes

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.master')
 
 @section('content')
-
 <!--USER PROFILE PAGE-->
 
 <div class="content-wrapper">
@@ -13,23 +12,49 @@
       </li>
     </ol>
 
-
-    <div class="row">
-      <div class="col-12">
-
-
-
-        <h1>Blank</h1>
-        <p>This is an example of a blank page that you can use as a starting point for creating new ones.</p>
-      
-      
-      
-      
-      
-      </div>
-    </div>
+  
+<div class="card bg-light border-dark mb-3" style="max-width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title"><i class="fa fa-fw fa-user"></i>Username</h5>
+    <p class="card-text">Some quick example text for the card's content.</p>
   </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item"> <i class="fa fa-fw fa-map-marker"></i><cite>Iasi, ROMANIA</cite></li>
+    <li class="list-group-item"> <i class="fa fa-fw fa-envelope"></i>email@example.com</li>
+    <li class="list-group-item"><i class="fa fa-fw fa-globe"></i><a href="http://facebook">www.facebook.com</a></li>
+    <li class="list-group-item"><i class="fa fa-fw fa-gift"></i>June 12, 1998</li>
+  </ul>
+  <div class="card-body">
+    <a href="#" class="btn btn-primary">Trimite mesaj</a>
+   <a href="#" class="btn btn-secondary">Adauga</a>
+  </div>
+</div>
 
+<div class="mb-0 mt-4">
+            <h5 class="card-title"><i class="fa fa-fw fa-newspaper-o"></i> &nbsp; Teme incarcate</h5>
+          <hr class="mt-2">
+
+
+<div class="card-columns">
+        <!-- Example Homework Card-->
+        <div class="card bg-light border-dark mb-3" style="width: 25rem">
+          <div class="card-body text">
+            <!--Homework title-->
+            <h5 class="card-title">Tema: Laborator 8</h5>
+          </div>
+          <div class="card-footer bg-transparent border">
+
+            &#10048; Descriere: blabla</div>
+
+          <div class="card-footer bg-transparent border">
+
+            &#10048; Categorie: SGBD</div>
+             <div class="card-body">
+          <a href="#" class="btn btn-primary">Detalii</a>
+          <a href="#" class="btn btn-secondary">Editeaza</a>
+        </div>
+      </div>
+      </div>
   <!-- /.container-fluid-->
   <!-- /.content-wrapper-->
 
