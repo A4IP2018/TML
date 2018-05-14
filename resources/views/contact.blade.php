294f873a5a6589747abeb76fa4146eb4d0dec154
ancab <ancabejinariu16@gmail.com>
2018-05-14 11:21:15 +0300

    Contact page changes

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -16,10 +16,29 @@
 
             <div class="row">
                 <div class="col-12">
-
-
-                    <h1>Blank</h1>
-                    <p>This is an example of a blank page that you can use as a starting point for creating new ones.</p>
+                    <br>
+                    <p class="text-center" style="font-size: 30px">Ai nelamuriri? Intreaba-ne!</p>
+                    <div class="card card-register mx-auto mt-5">
+                    <div class="card">
+                        <div class="card-body">
+                    <form action="/action_page.php">
+                        <div class="form-group">
+                            <label for="name">Name:</label>
+                            <input type="name" class="form-control" placeholder="Enter your name here" id="name">
+                        </div>
+                        <div class="form-group">
+                            <label for="email">Email address:</label>
+                            <input type="email" class="form-control" placeholder="Enter your e-mail address here" id="email">
+                        </div>
+                        <div class="form-group">
+                            <label for="pwd">Message:</label>
+                            <textarea id="form_message" name="message" class="form-control" placeholder="Enter your message here" rows="4" required="required" data-error="Please,leave us a message."></textarea>
+                        </div>
+                        <button type="send-message" class="btn btn-primary">Send message</button>
+                    </form>
+                        </div>
+                    </div>
+                    </div>
 
 
 
@@ -46,7 +65,8 @@
                 </div>
             </div>
         </div>
-
+        <br>
+    </div>
         <!-- /.container-fluid-->
         <!-- /.content-wrapper-->
 
