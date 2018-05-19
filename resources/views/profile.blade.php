7554fbc2bd954b4c2633b1194da0c1ad64eca1c9
NinjaSaph <andra.oica@yahoo.com>
2018-05-19 10:22:04 +0300

    Profile page settings

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -13,82 +13,78 @@
     </ol>
 
 <div class="row">
-    <div class="col-sm-4">
-    <div class="card bg-light border-dark mb-3" style="max-width: 18rem;">
-  <div class="card-body">
-    <h5 class="card-title"><i class="fa fa-fw fa-user"></i>Username</h5>
-    <p class="card-text">Some quick example text for the card's content.</p>
-  </div>
-  <ul class="list-group list-group-flush">
-    <li class="list-group-item"> <i class="fa fa-fw fa-map-marker"></i><cite>Iasi, ROMANIA</cite></li>
-    <li class="list-group-item"> <i class="fa fa-fw fa-envelope"></i>email@example.com</li>
-    <li class="list-group-item"><i class="fa fa-fw fa-globe"></i><a href="http://facebook">www.facebook.com</a></li>
-    <li class="list-group-item"><i class="fa fa-fw fa-gift"></i>June 12, 1998</li>
-  </ul>
-  <div class="card-body">
-    <a href="#" class="btn btn-primary">Trimite mesaj</a>
-   <a href="#" class="btn btn-secondary">Adauga</a>
-  </div>
-</div>
-</div>
-    <div class="col-sm-6">  
-    <div class="card  border-dark mb-3" style="max-width: 70rem;">
-            <div class="card-header">
-              <i class="fa fa-bar-chart"></i> Bar Chart</div>
-            <div class="card-body">
-              <div class="row">
-                <div class="col-sm-8 my-auto">
-                  <canvas id="myBarChart" width="100" height="70"></canvas>
+    <div class="col-12">
+
+        <div class="mb-0 mt-0">
+            <i class="fa fa-archive"></i> Setari cont</div>
+        <hr class="mt-0">
+        <div class="card card-register mx-auto mt-4">
+            <div class="card">
+                <div class="card-body">
+                    <form action="#">
+                        <div class="form-group">
+                            <label for="name">Nume:</label>
+                            <input type="name" class="form-control" id="name" value="Patlagica Ionut" readonly>
+                        </div>
+                        <div class="form-group">
+                            <label for="email">An:</label>
+                            <input type="email" class="form-control" id="an" value="2" readonly>
+                        </div>
+                        <div class="form-group">
+                            <label for="email">Nr. matricol:</label>
+                            <input type="email" class="form-control" id="nr_matr" value="37hfjshdf" readonly>
+                        </div>
+                        <label for="email">Adresa de mail:</label>
+                        <div class="input-group">
+                            <input type="email" class="form-control" id="email" value="patlagica.ion@yahoo.com" readonly>
+                            <span class="input-group-append">
+                                <button data-toggle="collapse" data-target="#changeEmail" class="btn btn-primary">Schimba <i class="fa fa-eraser"></i></button>
+                            </span>
+                        </div>
+                        <label for="password">Parola:</label>
+                        <div class="input-group">
+                            <input type="text" class="form-control" id="pass" value="*******" readonly>
+                            <span class="input-group-append">
+                                <button data-toggle="collapse" data-target="#changePassword" class="btn btn-primary">Schimba <i class="fa fa-eraser"></i></button>
+                            </span>
+                        </div>
+                        <div id="changePassword" class="collapse">
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="form-group">
+                                        <label for="name">Parola veche:</label>
+                                        <input type="name" class="form-control" id="oldPass" placeholder="Scrie vechea ta parola">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="name">Parola noua:</label>
+                                        <input type="name" class="form-control" id="newPass" placeholder="Scrie noua ta parola">
+                                    </div>
+                                    <button type="submit" class="btn btn-primary">Salveaza</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div id="changeEmail" class="collapse">
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="form-group">
+                                        <label for="name">Adresa de mail veche:</label>
+                                        <input type="name" class="form-control" id="oldMail" placeholder="Scrie vechea ta adresa de mail">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="name">Adresa de mail noua:</label>
+                                        <input type="name" class="form-control" id="newMail" placeholder="Scrie noua ta adresa de mail">
+                                    </div>
+                                    <button type="submit" class="btn btn-primary">Salveaza</button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
                 </div>
-                <div class="col-sm-4 text-center my-auto">
-                  <div class="h4 mb-0 text-primary">REZULTATE</div>
-                  <div class="small text-muted"></div>
-                  <hr>
-                  <div class="h4 mb-0 text-warning">REZULTATE</div>
-                  <div class="small text-muted"></div>
-                  <hr>
-                  <div class="h4 mb-0 text-success">REZULTATE</div>
-                  <div class="small text-muted"></div>
-                </div>
-              </div>
             </div>
-            <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
-          </div>
-          </div>
-</div>
-
-
-
-    
-<div class="mb-0 mt-4">
-            <h5 class="card-title"><i class="fa fa-fw fa-newspaper-o"></i> &nbsp; Teme incarcate</h5>
-          <hr class="mt-2">
-
-
-<div class="card-columns">
-        <!-- Example Homework Card-->
-        <div class="card bg-light border-dark mb-3" style="width: 25rem">
-          <div class="card-body text">
-            <!--Homework title-->
-            <h5 class="card-title">Tema: Laborator 8</h5>
-          </div>
-          <div class="card-footer bg-transparent border">
-
-            &#10048; Descriere: blabla</div>
-
-          <div class="card-footer bg-transparent border">
-
-            &#10048; Categorie: SGBD</div>
-             <div class="card-body">
-          <a href="#" class="btn btn-primary">Detalii</a>
-          <a href="#" class="btn btn-secondary">Editeaza</a>
         </div>
-      </div>
-      </div>
- 
 
-  
-
+</div>
+</div>
 
   
 @endsection
