0d06979f5ee62580d4e02d135924573cfd44479e
teodoratimcu <teodoratimcu@gmail.com>
2018-05-26 12:38:49 +0300

    Notifications changes

--- resources/views/notifications.blade.php
+++ resources/views/notifications.blade.php
@@ -14,10 +14,10 @@
             </ol>
       <div class="row">
         <div class="mb-2" style="width:100%"></div>
-        <div class="col-sm-6">
+        <div class="col-sm-12">
           <div class="card-header">
             <i class="fa fa-bell"></i>Notificările mele</div>
-          <div class="list-group list-group-flush-small " style="height:350px; overflow-y:scroll">
+          <div class="list-group list-group-flush-small " style="height:630px; overflow-y:scroll">
                 <hr class="mt-2">
                     <!--notifications list-->
                 <div class="media>
@@ -54,6 +54,37 @@
                                 </span>
                             <hr>
                             </li>
+                            
+                            <li class="list-group-item>
+                                <span class="text-success">
+                                    <i class="fa fa-envelope fa-fw"></i>Comentariu nou la
+                                    <a href="#">Tema 5</a> (materie: <a href="#">Programare Avansata</a>)
+                                </span>
+                            <hr>
+                            </li>
+                            <li class="list-group-item>
+                                <span class="text-success">
+                                    <i class="fa fa-arrow-up fa-fw"></i>O noua <a href="#">tema</a> a fost adaugata la materia <a href="#">Introducere in Criptografie</a>
+                                </span>
+                            <hr>
+                            </li>
+                            <li class="list-group-item>
+                                <span class="text-success">
+                                    <i class="fa fa-arrow-circle-o-up fa-fw"></i>Un nou comentariu a fost adaugat la o <a href="#">tema</a> incarcata de tine!
+                                </span>
+                            <hr>
+                            </li>
+                            <li class="list-group-item>
+                                <span class="text-success">
+                                     <i class="fa fa-calendar-times-o fa-fw"></i>Mai sunt 3 zile pana la termenul limita de postare a <a href="#">temei</a>! (materie: <a href="#">PSGBD</a>)
+                                </span>
+                            <hr>
+                            </li>
+                            <li class="list-group-item>
+                                <span class="text-success">
+                                    <i class="fa fa-check-square fa-fw"></i>Ai primit o nota la <a href="#">Tema 7</a>! (materie: <a href="#">PSGBD</a>)
+                                </span>
+                            <hr>
                             <li class="list-group-item>
                                 <span class="text-success">
                                     <i class="fa fa-dot-circle-o fa-fw"></i>...<a href="#">mai multe</a>
