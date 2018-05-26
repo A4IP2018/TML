c91b93882439308374f78aed27753c46c15d5702
Denis ROZIMOVSCHII <rozimovschii.denis@gmail.com>
2018-05-26 20:38:20 +0300

    Added debug version that doesnt send mail

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 <div class="row">
-  <div class="col col-6">
+  <div class="col col-md-6 col-12">
     <div class="card">
       <div class="card-header text-white bg-primary">Coduri secrete pentru profesor</div>
       <div class="card-body">
@@ -16,8 +16,8 @@
       <div class="card-footer">Se ofer&#259; unui utilizator pentru a ob&#539;ine privilegii de profesor la &#238;nregistrare</div>
     </div>
   </div>
-
-  <div class="col col-6">
+  <br>
+  <div class="col col-md-6 col-12">
     <div class="card">
       <div class="card-header text-white bg-primary">Coduri secrete pentru administrator</div>
       <div class="card-body">
@@ -30,6 +30,7 @@
       <div class="card-footer">Se ofer&#259; unui utilizator pentru a ob&#539;ine privilegii de administrator la &#238;nregistrare</div>
     </div>
   </div>
+  <br>
 </div>
 
 
