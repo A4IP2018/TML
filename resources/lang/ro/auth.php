f3c9e4c61433c832999a215e489326f0cc773fca
TeodorDom <claw_teodor@yahoo.com>
2018-05-28 01:26:01 +0300

    Translation (#104)
    * First part of the romanian translation.
    
    * Added some other changes
    
    * Finished translating to romanian.

---
+++ resources/lang/ro/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Credidențialele nu pot fi găsite în baza de date.',
+    'throttle' => 'Prea multe încercări de logare. Vă rugăm să reîncercați în :seconds secunde.',
+
+];
