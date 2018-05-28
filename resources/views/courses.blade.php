400f4e4762aee8c11e434e161768a7357b9fd0f6
mihneabigu <mihnea.bigu@yahoo.com>
2018-05-28 23:58:03 +0300

    Removed certain styling

--- resources/views/courses.blade.php
+++ resources/views/courses.blade.php
@@ -80,7 +80,7 @@
         <!--Multiple Courses-->
         <div class="mb-0 mt-4">
 
-            <div class="card-columns" style="column-count: 3">
+            <div class="card-columns">
             @foreach ($courses as $course)
                 <!-- Example Course Card-->
                 <form action="{{ url("/course/" . $course->slug . "/subscribe") }}" method="POST">
