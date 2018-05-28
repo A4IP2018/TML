0d6117827b09821488d7ee54b459c986871597d4
BoAndrei <bocsan.andrei@gmail.com>
2018-05-28 19:45:10 +0300

    Fix homework details (#126)
    * Homework detail fixes
    
    * Homework detail fixes

--- resources/views/homework-details.blade.php
+++ resources/views/homework-details.blade.php
@@ -123,8 +123,19 @@
             @foreach($comments as $comment)
                 <div class="card mb-3">
                     <div class="card-body">
-                        <h6 class="card-title mb-1"><a href="#">{{ $comment->user->student_information->last_name }} {{ $comment->user->student_information->first_name }}</a> <small> 13:50 </small>
-                        </h6>
+                        @if ($comment->user->student_information)
+                            <h6 class="card-title mb-1"><a
+                                        href="#">{{ $comment->user->student_information->last_name }} {{ $comment->user->student_information->first_name }}</a>
+                                <small> {{ $comment->created_at }}</small>
+                            </h6>
+
+                        @else
+                            <h6 class="card-title mb-1"><a
+                                        href="#">{{ $comment->user->teacher_information->name }}</a>
+                                <small> {{ $comment->created_at }}</small>
+                            </h6>
+
+                        @endif
                         <p class="card-text small">
                         {{ $comment->comment }}
                         </p>
